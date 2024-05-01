0eb5b1ffecb96dbd5e314589a436a00ac5f1cb5a
surya pratap <suryapratap0765@gmail.com>
2024-05-01 12:22:16 +0530

    updated profile/ID page

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,8 +1,10 @@
 export default function ProfileID ({params}: any) {
     return (
-        <main>
+        <main className="flex flex-col items-center justify-center min-h-screen py-2">
             <h1>Profile</h1>
-            <p className="text-3xl">Profile with ID: {params.id}</p>
+            <hr />
+            <p className="text-4xl">Profile page</p>
+            <span className=" p-2 ml-2 rounded bg-orange-500 text-black">Profile with ID: {params.id}</span>
         </main>
     )
 }
